85a80847f35bf976c354ddd05e669b12fb9b63a0
Bartuer Zhou <bazhou@microsoft.com>
2023-05-06 06:28:02 +0000

    file list check

--- emacs/20.04_28.2/package/ls_files.sh
+++ emacs/20.04_28.2/package/ls_files.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 apt-get update
 apt-get install -y --no-install-recommends parallel
-parallel --pipepart /opt/file_list.txt
+
+function file_exist {
+while IFS= read -r file; do
+    test ! -e "$file" && echo "'$file' not found"
+done
+}
+
+export -f file_exist
+# file_exist
+parallel --pipepart -a /opt/file_list.txt file_exist
--- emacs/20.04_28.2/package/run.sh
+++ emacs/20.04_28.2/package/run.sh
@@ -2,4 +2,5 @@
 # docker run -v /root/provision/emacs/20.04_28.2/package:/opt caapi/emacs:package_28.2_20.04 /opt/cp_bin.sh
 # docker run -v /root/provision/emacs/20.04_28.2/package:/opt ubuntu:20.04 /opt/bin_libs.sh
 # docker run -v /root/provision/emacs/20.04_28.2/package:/opt caapi/emacs:package_28.2_20.04 /opt/lib_deps.sh
+# curl https://uswvhd.blob.core.windows.net/learning/ubuntu/20.04/me/x86_64/emacs.tgz |tar zt > file_list.txt
 docker run -v /root/provision/emacs/20.04_28.2/package:/opt caapi/emacs:package_28.2_20.04 /opt/ls_files.sh
